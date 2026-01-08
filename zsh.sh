e3d0d85c14eb962d32896d4515c45f59de8b8a66
DeryKams <derykams@gmail.com>
2026-01-08 10:25:48 +1000

    add omzsh curl

--- zsh.sh
+++ zsh.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Обновляем систему
-pacman -Syu
 # устанавливаем zsh и дополнительные пакеты
-pacman -S --needed --noconfirm zsh zsh-completions git curl fzf powerline-fonts nerd-fonts-hack
+pacman -S --needed --noconfirm zsh zsh-completions git curl fzf powerline-fonts nerd-fonts-hack zsh-syntax-highlighting
+
 
 chsh -s $(which zsh)
 # chsh — это команда, которая меняет оболочку входа пользователя в систему
 echo $SHELL
+
+# Установка фреймворка Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
