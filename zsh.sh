16e1d9be363eb74e1f0cb8cc3c2878ad9e58799b
DeryKams <derykams@gmail.com>
2026-01-08 11:07:26 +1000

    testing just

--- zsh.sh
+++ zsh.sh
@@ -4,14 +4,15 @@
 pacman -S --needed --noconfirm git curl zsh fzf powerline-fonts zsh-syntax-highlighting zsh-autosuggestions zsh-completions
 
 
-chsh -s $(which zsh)
-# chsh — это команда, которая меняет оболочку входа пользователя в систему
-echo $SHELL
+
 
 # Установка фреймворка Oh My Zsh
 sudo -u "$SUDO_USER" bash -c "
 cd ~
 sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\" \"\" --unattended
+chsh -s $(which zsh)
+# chsh — это команда, которая меняет оболочку входа пользователя в систему
+echo $SHELL
 "
 # Необходимо экранировать кавычки внутри команды bash -c, если открывается с двойных кавычек
 
