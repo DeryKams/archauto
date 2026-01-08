03091099935cf0bd430d30814b9d8bcc2b877c4a
DeryKams <derykams@gmail.com>
2026-01-08 13:08:43 +1000

    added link on pacman plugins

--- zsh.sh
+++ zsh.sh
@@ -12,6 +12,8 @@ pacman -S --needed --noconfirm git curl zsh fzf powerline-fonts zsh-syntax-highl
 sudo -u "$SUDO_USER" bash -c "
 cd ~
 sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\" \"\" --unattended
+chsh -s $(which zsh)
+# chsh — это команда, которая меняет оболочку входа пользователя в систему
 "
 # Необходимо экранировать кавычки внутри команды bash -c, если открывается с двойных кавычек
 
@@ -20,6 +22,13 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $USER_HOME/.oh-
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $USER_HOME/.oh-my-zsh/custom/plugins/you-should-use
 git clone https://github.com/Aloxaf/fzf-tab $USER_HOME/.oh-my-zsh/custom/plugins/fzf-tab
 # Replace zsh's default completion selection menu with fzf
+
+sudo -u "$SUDO_USER" bash << 'EOF'
+# Создаем симлинки на системные плагины
+ln -sf /usr/share/zsh/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
+ln -sf /usr/share/zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+ln -sf /usr/share/zsh/plugins/zsh-completions ~/.oh-my-zsh/custom/plugins/
+EOF
 
 if [[ -f  $USER_HOME/.zshrc ]]; then  
 # изменяем тему в .zshrc на powerlevel10k
@@ -30,7 +39,5 @@ else
     echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> $USER_HOME/.zshrc
 fi
 
-chsh -s $(which zsh)
-# chsh — это команда, которая меняет оболочку входа пользователя в систему
 
 echo "Для вступления изменений в силу, перезайдите в систему или выполните команду: exec zsh"
