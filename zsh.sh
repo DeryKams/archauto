a7dcbaa954f5a3bc3a25ab549fb239775836b876
DeryKams <derykams@gmail.com>
2026-01-08 11:24:31 +1000

    adding theme for zsh

--- zsh.sh
+++ zsh.sh
@@ -16,5 +16,13 @@ echo $SHELL
 "
 # Необходимо экранировать кавычки внутри команды bash -c, если открывается с двойных кавычек
 
+# Установка темы Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+if [[ -f "~/.zshrc" ]]; then
+    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+else
+    echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+fi
 
 echo "Для вступления изменений в силу, перезайдите в систему или выполните команду: exec zsh"
