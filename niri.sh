b628c48d3706677609528ec03bdab4ba65ba164a
DeryKams <derykams@gmail.com>
2026-04-21 09:14:50 +1000

    deleting quickshell and replace download noctalia

--- niri.sh
+++ niri.sh
@@ -455,46 +455,64 @@ fi
 # нужно удалить quickshell, так как он конфликтует с noctalia
 sudo pacman -Rns --noconfirm quickshell quickshell-git
 #Установка пакетов из AUR
-# paru -S --needed --noconfirm alacritty fuzzel mako niri neowall-git swayidle swaylock wl-clipboard-history-git xdg-desktop-portal-gnome xorg-xwayland  xwayland-satellite matugen  cava dms-shell-niri qt6-multimedia-ffmpeg noctalia-shell-git noctalia-qs-git pcmanfm-qt gvfs qt6ct kvantum nohang-git aur/minq-ananicy-git aur/stacer-bin xdman8-beta-git firefox-extension-xdman8-browser-monitor-bin aur/php-codesniffer-phpcsutils aur/php-codesniffer-phpcsextra visual-studio-code-bin fastfetch-git 
+paru -S --needed --noconfirm alacritty fuzzel mako niri neowall-git swayidle swaylock wl-clipboard-history-git xdg-desktop-portal-gnome xorg-xwayland  xwayland-satellite matugen  cava dms-shell-niri qt6-multimedia-ffmpeg noctalia-shell-git noctalia-qs-git pcmanfm-qt gvfs qt6ct kvantum nohang-git aur/minq-ananicy-git aur/stacer-bin xdman8-beta-git firefox-extension-xdman8-browser-monitor-bin aur/php-codesniffer-phpcsutils aur/php-codesniffer-phpcsextra visual-studio-code-bin fastfetch-git 
 #Установка пакетов из AUR
 # aur/neowall-git можно заменить на swaybg or swww-daemon
-# waybar
-USER_NAME="${SUDO_USER:?Запускай через sudo от обычного пользователя}"
-SUDOERS_FILE="/etc/sudoers.d/99-paru-${USER_NAME}"
+# # waybar
+# USER_NAME="${SUDO_USER:?Запускай через sudo от обычного пользователя}"
+# SUDOERS_FILE="/etc/sudoers.d/99-paru-${USER_NAME}"
 
-# Даём пользователю право ставить пакеты через pacman без второго пароля
-printf '%s ALL=(ALL) NOPASSWD: /usr/bin/pacman, /usr/bin/pacman-key\n' "$USER_NAME" > "$SUDOERS_FILE"
-chmod 440 "$SUDOERS_FILE"
-visudo -cf "$SUDOERS_FILE" >/dev/null
+# # Даём пользователю право ставить пакеты через pacman без второго пароля
+# printf '%s ALL=(ALL) NOPASSWD: /usr/bin/pacman, /usr/bin/pacman-key\n' "$USER_NAME" > "$SUDOERS_FILE"
+# chmod 440 "$SUDOERS_FILE"
+# visudo -cf "$SUDOERS_FILE" >/dev/null
 
-# В конце всегда удаляем временное правило
-trap 'rm -f "$SUDOERS_FILE"' EXIT
+# # В конце всегда удаляем временное правило
+# trap 'rm -f "$SUDOERS_FILE"' EXIT
 
-# Запускаем paru от обычного пользователя
-# --skipreview убирает "Proceed to review?"
-# --sudoloop удерживает sudo-сессию во время долгой сборки
-sudo -u "$USER_NAME" paru -S --needed --noconfirm --skipreview --sudoloop \
-    alacritty fuzzel mako niri neowall-git swayidle swaylock wl-clipboard-history-git xdg-desktop-portal-gnome xorg-xwayland  xwayland-satellite matugen  cava dms-shell-niri qt6-multimedia-ffmpeg noctalia-shell-git pcmanfm-qt gvfs qt6ct kvantum nohang-git aur/minq-ananicy-git aur/stacer-bin xdman8-beta-git firefox-extension-xdman8-browser-monitor-bin aur/php-codesniffer-phpcsutils aur/php-codesniffer-phpcsextra visual-studio-code-bin fastfetch-git 
+# # Запускаем paru от обычного пользователя
+# # --skipreview убирает "Proceed to review?"
+# # --sudoloop удерживает sudo-сессию во время долгой сборки
+# sudo -u "$USER_NAME" paru -S --needed --noconfirm --skipreview --sudoloop \
+#     alacritty fuzzel mako niri neowall-git swayidle swaylock wl-clipboard-history-git xdg-desktop-portal-gnome xorg-xwayland  xwayland-satellite matugen  cava dms-shell-niri qt6-multimedia-ffmpeg noctalia-shell-git pcmanfm-qt gvfs qt6ct kvantum nohang-git aur/minq-ananicy-git aur/stacer-bin xdman8-beta-git firefox-extension-xdman8-browser-monitor-bin aur/php-codesniffer-phpcsutils aur/php-codesniffer-phpcsextra visual-studio-code-bin fastfetch-git 
 
 
 # Установка и настройка greetd для входа в niri
-pacman -S --needed --noconfirm greetd greetd-tuigreet
-echo "Настраиваем greetd для входа в niri"
+pacman -S --needed --noconfirm greetd greetd-regreet cage
 
+echo "Настраиваем greetd для входа через ReGreet"
+
+# Включаем сервис логин-менеджера
 systemctl enable greetd.service
 
-cat > /etc/greetd/config.toml <<'EOF'
-[terminal]
-vt = 1
+cat > /etc/greetd/regreet.toml <<'EOF'
+[background]
+path = "/usr/share/backgrounds/greeter.jpg"
+fit = "Cover"
 
-[default_session]
-command = "tuigreet --time --remember --remember-session --user-menu --cmd niri-session"
-user = "greeter"
+[GTK]
+application_prefer_dark_theme = true
+cursor_theme_name = "Adwaita"
+cursor_blink = true
+font_name = "Inter 12"
+icon_theme_name = "Adwaita"
+theme_name = "Adwaita"
+
+[appearance]
+greeting_msg = "Welcome"
+
+[commands]
+reboot = ["systemctl", "reboot"]
+poweroff = ["systemctl", "poweroff"]
+
+[widget.clock]
+format = "%a %H:%M"
+resolution = "500ms"
+label_width = 150
 EOF
 
 # Установка и настройка greetd для входа в niri
 
-# TEST Добавляем значения конфига
 
 #TEST Добавляем значения конфига
 
